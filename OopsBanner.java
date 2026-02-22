2440fbc1147c1a16ff18c315cd2f3377d0ccaaae
ak4003-cmd <ak4003@srmist.edu.in>
2026-02-22 22:39:07 +0530

    first commit

---
+++ OopsBanner.java
@@ -0,0 +1,12 @@
+public class OopsBanner {
+    public static void main(String[] args) {
+        // UC2: Rendering "OOPS" using a 7-line grid and manual print statements
+        System.out.println("  *** *** **** **** ");
+        System.out.println(" * * * * * * * ");
+        System.out.println(" * * * * * * * ");
+        System.out.println(" * * * * **** **** ");
+        System.out.println(" * * * * * *");
+        System.out.println(" * * * * * *");
+        System.out.println("  *** *** * **** ");
+    }
+}
